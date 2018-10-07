99fa92a20fe8f8edf7e99d977aec280a95f4aaf4
alex <alexandru.horsa@gmail.com>
2018-10-08 01:33:09 +0300

    Refactoring view model to cache data. TODO fix loading indicator when data already loaded.

--- app/src/main/java/com/sample/githubapp/MainViewModel.kt
+++ app/src/main/java/com/sample/githubapp/MainViewModel.kt
@@ -9,11 +9,30 @@ import io.reactivex.schedulers.Schedulers
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
+    private var cacheGithubRepos: HashMap<GithubClient.TrendingPeriod, List<GithubRepo>> = HashMap()
+
     fun getTrendingRepositories(period: GithubClient.TrendingPeriod): Observable<List<GithubRepo>> {
+        return Observable.concat(
+                getLocalTrendingRepositories(period),
+                getRemoteTrendingRepositories(period)
+        ).filter { list -> !list.isEmpty()}
+    }
+
+    private fun getRemoteTrendingRepositories(period: GithubClient.TrendingPeriod): Observable<List<GithubRepo>> {
         val githubClient = ServiceGenerator.createService(GithubClient::class.java)
         return githubClient
                 .fetchTrendingRepos(period.periodString)
+                .doOnNext { repoList -> cacheGithubRepos[period] = repoList }
                 .subscribeOn(Schedulers.io())
     }
 
+    private fun getLocalTrendingRepositories(period: GithubClient.TrendingPeriod): Observable<List<GithubRepo>> {
+        val repoList = if (cacheGithubRepos.containsKey(period)) {
+            cacheGithubRepos[period]
+        } else {
+            ArrayList()
+        }
+
+        return Observable.just(repoList)
+    }
 }
